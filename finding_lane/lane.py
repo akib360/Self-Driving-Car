b6c8c86444395fae619d64241d14c2266d67bc04
akib hosen khan <36074470+akib360@users.noreply.github.com>
2019-09-12 23:03:04 +0600

    Update lane.py

--- finding_lane/lane.py
+++ finding_lane/lane.py
@@ -4,8 +4,8 @@ import numpy as np
 #[5th function] finally coordinate making
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
-    y1 = image.shape[0]
-    y2 = int(y1*(3/5))
+    y1 = image.shape[0] # bottom of the image
+    y2 = int(y1*(3/5))  # slightly lower than the middle
     x1 = int((y1-intercept)/slope)
     x2 = int((y2-intercept)/slope)
     return np.array([x1, y1, x2, y2])
@@ -19,10 +19,11 @@ def average_slope_intercept(image, lines):
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
-        if slope < 0:
+        if slope < 0:    # y is reversed in image
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
+    # add more weight to longer lines
     left_fit_average = np.average(left_fit, axis=0)
     right_fit_average = np.average(right_fit, axis=0)
     left_line = make_coordinates(image, left_fit_average)
